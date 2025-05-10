05210f88e2104681f0cd3c3e7aeee09d032ba5c5
cauefer <cauefernandescaetano@gmail.com>
2025-05-10 02:01:33 -0300

    ` wip `:  Comeco da funcao de criar novo chat

--- src/components/ui/navs/leftBarContent.tsx
+++ src/components/ui/navs/leftBarContent.tsx
@@ -4,12 +4,19 @@ import { Tooltip } from "@nextui-org/react";
 
 import { Button as ButtonSd } from "@/components/ui/button";
 import { User } from "@/lib/global.types";
+import { useRouter } from "next/navigation";
 
 interface LeftBarContentProps {
   smallMenu: boolean;
   user: User | null;
 }
 export function LeftBarContent({ smallMenu, user }: LeftBarContentProps) {
+  const router = useRouter();
+
+  const handleNewChat = () => {
+    router.push("/chat");
+  };
+
   return (
     <NavbarItem className="max-w-full flex flex-col text-center justify-center items-center truncate gap-4">
       <Tooltip
@@ -54,7 +61,10 @@ export function LeftBarContent({ smallMenu, user }: LeftBarContentProps) {
         </>
       )}
       {!smallMenu && user && (
-        <span className="group flex flex-row justify-center items-center px-4 py-2 rounded-md border border-muted hover:bg-muted/20 cursor-pointer shadow min-w-[170px]">
+        <span
+          className="group flex flex-row justify-center items-center px-4 py-2 rounded-md border border-muted hover:bg-muted/20 cursor-pointer shadow min-w-[170px] select-none"
+          onClick={() => handleNewChat()}
+        >
           <DiamondPlus className="w-5 h-4 mr-2 group-hover:rotate-90 transition-transform duration-700 ease-in-out" />
           Nova conversa
         </span>
